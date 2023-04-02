99c7849234e6eedc0bb9d147792e99434b80191b
Bruno Faria <bruno.faria@drpaysaude.com.br>
2023-04-02 15:39:46 -0300

    deploy

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 
 connectToDb();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 var allowCrossDomain = function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- database/db.js
+++ database/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectToDb = () => {
   mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      "mongodb+srv://root:admin@cluster0.wzgzqxr.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => console.log("MongoDB Atlas Conectado"))
     .catch((err) => console.log(err));
 };
